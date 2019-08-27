16663d48b4fa70d24f0c4ff14886eed85ccf8b34
danylaksono <danylaksono@ugm.ac.id>
2019-08-28 00:40:45 +0700

    revert popup

--- src/app/service/highlightfeature.service.ts
+++ src/app/service/highlightfeature.service.ts
@@ -1,28 +1,59 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef, ViewChild } from '@angular/core';
 import Popup from 'ol-popup';
-import { Fill, Stroke, Style, Text } from 'ol/style';
+import { Fill, Stroke, Style } from 'ol/style';
 import OlVectorSource from 'ol/source/Vector';
 import OlVectorLayer from 'ol/layer/Vector';
 import OlGeoJSON from 'ol/format/GeoJSON';
 
 import { WarningSnackbarService } from './../dialog/warning-snackbar.service';
+import { Overlay } from 'ol';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HighlightfeatureService {
+  @ViewChild('popup', { static: false }) container: ElementRef;
+  @ViewChild('popupcloser', { static: false }) closer: ElementRef;
+  @ViewChild('popupcontent', { static: false }) content: ElementRef;
 
   popup: any;
   VectorLayer: OlVectorLayer;
+  overlay: Overlay;
 
   constructor(
     private warning: WarningSnackbarService
   ) { }
 
+
+  
+  
   
   checkFeature(features, coordinate, map) {
     this.popup = new Popup();
     map.addOverlay(this.popup);
+
+    /*
+
+    this.overlay = new Overlay({
+      element: this.container.nativeElement,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250
+      }
+    });
+
+    this.closer.nativeElement.onclick = function() {
+      this.overlay.setPosition(undefined);
+      this.closer.blur();
+      return false;
+    };
+
+    map.addOverlay(this.overlay);
+
+    this.content.nativeElement.innerHTML = '<p>You clicked here:</p><code>'+'the hash slinging' +  '</code>';
+    */
+
+    
     
     var ketRDTR = features.features[0].properties;
     var ketBidang = features.features[1].properties;
@@ -34,6 +65,7 @@ export class HighlightfeatureService {
       } else if (features.numberReturned < 2) {
         this.warning.open('Data bidang tanah atau RDTR tidak tersedia');
       } else {       
+        
         this.popup.show(coordinate, 
         '<h3>Bidang Tanah </h3>' + '<br>' +
         'Zona RDTR: '+  ketRDTR.zona +  '<br>' +
@@ -42,6 +74,8 @@ export class HighlightfeatureService {
          '<button mat-raised-button color="primary" (click)=check()> Cek Bidang Ini </button>'
           
           );
+          
+        //this.overlay.setPosition(coordinate);
         this.highlightSelected(features, map);
       }
     }
